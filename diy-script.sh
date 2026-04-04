3efc10d0d2bb968ae3f458675e14566d82ee2470
NWCHASER <112234001+NWCHASER@users.noreply.github.com>
2026-04-04 19:52:39 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# 删除 Mediatek 和其他非 x86 架构的源码目录
+echo "正在清理非 x86_64 架构的源码..."
+rm -rf target/linux/mediatek
+rm -rf target/linux/ramips
+rm -rf target/linux/rockchip
+rm -rf target/linux/bcm27xx
+rm -rf target/linux/ipq806x
+rm -rf target/linux/ipq40xx
+# 可以根据需要添加更多不需要的架构
+echo "清理完成。"
+
 # 修改默认IP
 # sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
